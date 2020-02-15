593c9ae065a23aba4caae5e50c9d11f39049c4c3
franckie <francklee85@gmail.com>
2020-02-15 12:06:38 +0800

    first commit

---
+++ src/test/java/com/franckie/community/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franckie.community.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
